62ef206d1b031aac9803abdd4ad9c8ce9c7e01d9
Dragonkid1996 <djnewell@btinternet.com>
2020-10-10 17:09:05 +0100

    Add new properties for person class

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -6,5 +6,8 @@ namespace TeamPowered
 {
     class Person
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
     }
 }
